5b3e5abe5851dacbde9d664d9887154c4565edd7
Boo-coder <Bouna112@yahoo.com>
2024-11-15 22:35:50 -0500

    fstclsfnc

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function() {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function() {
+    return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fourMultiplier) {
+    return function(fare) {
+        return fare * fourMultiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+}   
